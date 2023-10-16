e7071c5cb2a204ea535bb0042f17c675ef629141
Esteban Casallas <ecasallas07@gmail.com>
2023-10-15 21:46:20 -0500

    design the name

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-const hexaColor = ()
+const title = document.querySelectorAll('.shake_it'); //all elementos
+const username = document.querySelector('.t__username'); //one element is very especific
+const greeting = document.querySelector('.t__hello');
+const textAutomatically = document.getElementById('t__text');
+const aboutme = document.querySelector('.t__aboutme');
+let text = 'Developer,Cibersecurity and IA';
+let textAboutme ='About me'
+
+// if used querySelectorAll is for loop through the element 
+
+let colorChange = false;
+function changeColor() {
+    if(colorChange){
+        greeting.style.color = 'grey'
+    }else{
+        greeting.style.color = 'rgb(136, 136, 218)'
+    }
+
+    if(colorChange){
+        username.style.background = 'rgb(136, 136, 218)';
+    }else{
+        username.style.background = 'grey';
+    }
+    // TODO: Caundo viene verdadero, se cambia a falso, y cuando viene falso se vuelve verdadero
+    //el signo de admiracion es un operador de negacion
+    colorChange = !colorChange;
+}
+
+setInterval(() => {
+    greeting.style.color = changeColor()
+    username.style.fontWeight = 'bold';
+},1000)
+
+let index = 0;
+function writter() {
+    if(index < text.length) {
+        textAutomatically.textContent += text.charAt(index); // charArt es un metodo para acceder a un caracter especifico de una cadena de texto se le pasa el indice
+        index++;
+    }else{
+        clearInterval();
+    }
+
+}
+
+const interval = setInterval(writter, 100);
+
+
+
